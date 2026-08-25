7c43c1b392d5b6a96a99bf3058cc7c51068e6932
tom <tomsperry@gmail.com>
2026-08-25 14:46:21 -0400

    chirp table

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 	"os"
 	"strings"
 
+	"time"
 	//	"net/textproto"
 	"sync/atomic"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/tsperry/chirpy/internal/database"
@@ -20,6 +22,14 @@ import (
 type apiConfig struct {
 	fileServerHits atomic.Int32
 	queries        database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (config *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -49,13 +59,25 @@ func (config *apiConfig) requestCounter(w http.ResponseWriter, req *http.Request
 	w.Write([]byte(hits))
 }
 
-func (config *apiConfig) resetCounter(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	config.fileServerHits.Store(0)
-	hits := fmt.Sprintf("Hits: %v", config.fileServerHits.Load())
+func (config *apiConfig) resetCounter(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte(hits))
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	if config.platform == "dev" {
+		w.WriteHeader(http.StatusOK)
+		config.fileServerHits.Store(0)
+		hits := fmt.Sprintf("Hits: %v", config.fileServerHits.Load())
+
+		err := config.queries.DeleteUsers(r.Context())
+
+		if err != nil {
+			log.Printf("error deleting users table: %s", err)
+		}
+
+		w.Write([]byte(hits))
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 
 func replaceBadWords(s string) string {
@@ -77,10 +99,53 @@ func replaceBadWords(s string) string {
 
 }
 
-func (config *apiConfig) chirpValidator(w http.ResponseWriter, r *http.Request) {
+func (config *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+
+	type userEmail struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	user := userEmail{}
+	err := decoder.Decode(&user)
+
+	if err != nil {
+		log.Printf("Error parsing email: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/josn; charset=utf-8")
+	w.WriteHeader(201)
+
+	newUser, err := config.queries.CreateUser(r.Context(), user.Email)
+
+	if err != nil {
+		log.Printf("error creating new user: %s", err)
+	}
+
+	jsonUser := User{
+		ID:        newUser.ID,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+		Email:     newUser.Email,
+	}
+
+	jsonData, err := json.Marshal(jsonUser)
+
+	if err != nil {
+		log.Printf("error marshalling json: %s", err)
+	}
+
+	w.Write(jsonData)
+
+}
+
+func (config *apiConfig) chirpHandler(w http.ResponseWriter, r *http.Request) {
 
 	type chirpBody struct {
-		Body string `json:"body"`
+		Body   string `json:"body"`
+		userID string `json:"user_id"`
 	}
 
 	type errorBody struct {
@@ -124,13 +189,13 @@ func (config *apiConfig) chirpValidator(w http.ResponseWriter, r *http.Request) 
 
 		cleaned_chirp.Cleaned_body = replaceBadWords(chirp.Body)
 
-		jsonData, err := json.Marshal(cleaned_chirp)
+		dat, err := json.Marshal(cleaned_chirp)
 
 		if err != nil {
 			log.Printf("error marshalling json: %s", err)
 		}
 
-		w.Write(jsonData)
+		w.Write(dat)
 
 	}
 }
@@ -139,6 +204,7 @@ func main() {
 
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 
 	db, err := sql.Open("postgres", dbURL)
 
@@ -158,6 +224,7 @@ func main() {
 	var config apiConfig
 
 	config.queries = *dbQueries
+	config.platform = platform
 
 	mux.Handle("/app/", (&config).middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
@@ -166,6 +233,8 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", (&config).requestCounter)
 	mux.HandleFunc("POST /admin/reset", (&config).resetCounter)
 	mux.HandleFunc("POST /api/validate_chirp", (&config).chirpValidator)
+	mux.HandleFunc("POST /api/chirps", (&config).chirpHandler)
+	mux.HandleFunc("POST /api/users", (&config).createUser)
 
 	server.ListenAndServe()
 
